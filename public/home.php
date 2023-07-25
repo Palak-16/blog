06045656ac42df7c939e214e88717958e6c18346
Palak-16 <79013214+Palak-16@users.noreply.github.com>
2023-07-25 19:50:20 +0530

    updated home

--- public/home.php
+++ public/home.php
@@ -68,73 +68,78 @@
                 </div>
             </div>
         </header>
-        
+
         <!--slider-->
-        <link rel="stylesheet" href="assets/slider/ism/css/my-slider.css"/>
+        <link rel="stylesheet" href="assets/slider/ism/css/my-slider.css" />
         <script src="assets/slider/ism/js/ism-2.2.min.js"></script>
 
 
         <div class="ism-slider" data-transition_type="fade" data-play_type="loop" id="my-slider">
             <ol>
-              <li>
-                <img src="assets/slider/ism/image/slides/flower-729514_1280.jpg">
-                <div class="ism-caption ism-caption-0">My slide caption text</div>
-              </li>
-              <li>
-                <img src="assets/slider/ism/image/slides/beautiful-701678_1280.jpg">
-                <div class="ism-caption ism-caption-0">My slide caption text</div>
-              </li>
-              <li>
-                <img src="assets/slider/ism/image/slides/summer-192179_1280.jpg">
-                <div class="ism-caption ism-caption-0">My slide caption text</div>
-              </li>
-              <li>
-                <img src="assets/slider/ism/image/slides/city-690332_1280.jpg">
-                <div class="ism-caption ism-caption-0">My slide caption text</div>
-              </li>
+                <li>
+                    <img src="assets/slider/ism/image/slides/flower-729514_1280.jpg">
+                    <div class="ism-caption ism-caption-0">My slide caption text</div>
+                </li>
+                <li>
+                    <img src="assets/slider/ism/image/slides/beautiful-701678_1280.jpg">
+                    <div class="ism-caption ism-caption-0">My slide caption text</div>
+                </li>
+                <li>
+                    <img src="assets/slider/ism/image/slides/summer-192179_1280.jpg">
+                    <div class="ism-caption ism-caption-0">My slide caption text</div>
+                </li>
+                <li>
+                    <img src="assets/slider/ism/image/slides/city-690332_1280.jpg">
+                    <div class="ism-caption ism-caption-0">My slide caption text</div>
+                </li>
             </ol>
         </div>
         <!--end slider-->
-
+        <link href="assets/css/blog.css" rel="stylesheet">
+        <link href="assets/css/blog.rtl.css" rel="stylesheet">
 
         <main class="p-2">
-            <h3>Featured</h3>
+            <h3 class="mx-4">Featured</h3>
 
             <div class="row mb-2">
-              <div class="col-md-6">
-                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                  <div class="col p-4 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-primary-emphasis">World</strong>
-                    <h3 class="mb-0">Featured post</h3>
-                    <div class="mb-1 text-body-secondary">Nov 12</div>
-                    <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
-                        Continue reading
-                        <svg class="bi"><use xlink:href="#chevron-right"/></svg>
-                    </a>
-                    </div>
-                    <div class="col-lg-5 col-12 d-lg-block">
-                    <img class="bd-placeholder-img w-100" width="200" height="250" style="object-fit:cover;" src="assets\images\logo.jpg">
-                  </div>
-                </div>
-              </div>
-            <div class="col-md-6">
-              <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                 <div class="col p-4 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-success-emphasis">Design</strong>
-                    <h3 class="mb-0">Post title</h3>
-                    <div class="mb-1 text-body-secondary">Nov 11</div>
-                    <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
-                        Continue reading
-                        <svg class="bi"><use xlink:href="#chevron-right"/></svg>
-                    </a>
-                    </div>
-                    <div class="col-lg-5 col-12 d-lg-block">
-                    <img class="bd-placeholder-img w-100" width="200" height="250" style="object-fit:cover;" src="assets\images\logo.jpg">
+                <div class="col-md-6">
+                    <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                        <div class="col p-4 d-flex flex-column position-static">
+                            <strong class="d-inline-block mb-2 text-primary-emphasis">World</strong>
+                            <h3 class="mb-0">Featured post</h3>
+                            <div class="mb-1 text-body-secondary">Nov 12</div>
+                            <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
+                                Continue reading
+                                <svg class="bi">
+                                    <use xlink:href="#chevron-right" />
+                                </svg>
+                            </a>
+                        </div>
+                        <div class="col-lg-5 col-12 d-lg-block">
+                            <img class="bd-placeholder-img w-100" width="200" height="250" style="object-fit:cover;" src="assets\images\logo.jpg">
+                        </div>
                     </div>
                 </div>
-              </div>
+                <div class="col-md-6">
+                    <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                        <div class="col p-4 d-flex flex-column position-static">
+                            <strong class="d-inline-block mb-2 text-success-emphasis">Design</strong>
+                            <h3 class="mb-0">Post title</h3>
+                            <div class="mb-1 text-body-secondary">Nov 11</div>
+                            <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
+                                Continue reading
+                                <svg class="bi">
+                                    <use xlink:href="#chevron-right" />
+                                </svg>
+                            </a>
+                        </div>
+                        <div class="col-lg-5 col-12 d-lg-block">
+                            <img class="bd-placeholder-img w-100" width="200" height="250" style="object-fit:cover;" src="assets\images\logo.jpg">
+                        </div>
+                    </div>
+                </div>
             </div>
         </main>
 
@@ -152,8 +157,9 @@
                 <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
                     <li class="ms-3">
                         <a class="text-body-secondary" href="#">
-                             <i class="bi-twitter"></i>
-                        </a></li>
+                            <i class="bi-twitter"></i>
+                        </a>
+                    </li>
                     <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24">
                                 <use xlink:href="#instagram" />
                             </svg></a></li>
