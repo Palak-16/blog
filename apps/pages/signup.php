6b9c262ecc6424caf02e40e6cbf1fbd3bc410d7e
Palak-16 <79013214+Palak-16@users.noreply.github.com>
2023-07-25 11:00:53 +0530

    signup and login page layout done

--- apps/pages/signup.php
+++ apps/pages/signup.php
@@ -115,24 +115,36 @@
 <main class="form-signin w-100 m-auto">
   <form>
     <img class="mb-4" src="../../public/assets/images/logo.jpg" alt="" width="72" height="57">
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+    <h1 class="h3 mb-3 fw-normal">Create account</h1>
 
     <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <input name="username" type="text" class="form-control mb-2" id="floatingInput" placeholder="username">
+      <label for="floatingInput">Username</label>
+    </div>
+    <div class="form-floating">
+      <input name="email" type="email" class="form-control mb-2" id="floatingInput" placeholder="name@example.com">
       <label for="floatingInput">Email address</label>
     </div>
     <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
       <label for="floatingPassword">Password</label>
     </div>
+    <div class="form-floating">
+      <input name="retype_password" type="password" class="form-control" id="floatingPassword" placeholder="Retype Password">
+      <label for="floatingPassword">Retype Password</label>
+    </div>
+    <div class="my-2">
+      Don't have an account?
+      <a href="../../public/login.php">Login here</a>
+     </div>
 
     <div class="form-check text-start my-3">
-      <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+      <input name="terms" class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
       <label class="form-check-label" for="flexCheckDefault">
-        Remember me
+        Accept terms and conditions
       </label>
     </div>
-    <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+    <button class="btn btn-primary w-100 py-2" type="submit">Create </button>
   </form>
 </main>
 <script src="/assets/dist/js/bootstrap.bundle.min.js"></script>
--- public/login.php
+++ public/login.php
@@ -109,21 +109,26 @@
 
     
 <main class="form-signin w-100 m-auto">
-  <form>
+  <form method="post">
     <img class="mb-4" src="./assets/images/logo.jpg" alt="" width="72" height="57" style="object-fit: cover;">
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
     <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <input name="email" type="email" class="form-control mb-2" id="floatingInput" placeholder="name@example.com">
       <label for="floatingInput">Email address</label>
     </div>
     <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
       <label for="floatingPassword">Password</label>
     </div>
+    
+     <div class="my-2">
+      Don't have an account?
+      <a href="../apps/pages/signup.php">Signup here</a>
+     </div>
 
     <div class="form-check text-start my-3">
-      <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+      <input name="remember" class="form-check-input" type="checkbox" value="1" id="flexCheckDefault">
       <label class="form-check-label" for="flexCheckDefault">
         Remember me
       </label>
