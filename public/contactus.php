b67da41d1c0cc8a4779bba47693bb2d2a26a6c68
gauravbhandari07 <gauravbhandari282000@gmail.com>
2023-08-01 12:52:44 +0530

    contact us page

---
+++ public/contactus.php
@@ -0,0 +1,593 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <style>
+
+        .arch_contact_us_duplicate * {
+        font-family: Nunito, sans-serif;
+        }
+
+        .arch_contact_us_duplicate .icon-block svg {
+        width: 100%;
+        height: 100%;
+        }
+
+        .arch_contact_us_duplicate .icon-block {
+        width: 30px;
+        height: 30px;
+        }
+
+        .arch_contact_us_duplicate .text-blk {
+        margin-top: 0px;
+        margin-right: 0px;
+        margin-bottom: 0px;
+        margin-left: 0px;
+        padding-top: 0px;
+        padding-right: 0px;
+        padding-bottom: 0px;
+        padding-left: 0px;
+        line-height: 25px;
+        }
+
+        .arch_contact_us_duplicate .responsive-container-block {
+        min-height: 75px;
+        height: fit-content;
+        width: 100%;
+        padding-top: 0px;
+        padding-right: 0px;
+        padding-bottom: 0px;
+        padding-left: 0px;
+        display: flex;
+        flex-wrap: wrap;
+        margin-top: 0px;
+        margin-right: auto;
+        margin-bottom: 0px;
+        margin-left: auto;
+        justify-content: flex-start;
+        }
+
+        .arch_contact_us_duplicate .responsive-cell-block {
+        min-height: 75px;
+        }
+
+        .arch_contact_us_duplicate .responsive-container-block.big-container {
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+        padding-top: 0px;
+        padding-right: 50px;
+        padding-bottom: 0px;
+        padding-left: 50px;
+        background-color: rgba(0, 0, 0, .1);
+        }
+
+        .arch_contact_us_duplicate .responsive-container-block.container {
+        max-width: 1100px;
+        justify-content: space-evenly;
+        }
+
+        .arch_contact_us_duplicate .text-blk.section-head {
+        font-size: 60px;
+        line-height: 65px;
+        margin-top: 0px;
+        margin-right: 0px;
+        margin-bottom: 24px;
+        margin-left: 0px;
+        }
+
+        .arch_contact_us_duplicate .text-blk.section-subhead {
+        font-size: 14px;
+        line-height: 18px;
+        color: solid rgba(0, 0, 0, .15);
+        }
+
+        .arch_contact_us_duplicate .input {
+        width: 100%;
+        margin-top: 0px;
+        margin-right: 0px;
+        margin-bottom: 15px;
+        margin-left: 0px;
+        height: 54px;
+        border-top-left-radius: 5px;
+        border-top-right-radius: 5px;
+        border-bottom-right-radius: 5px;
+        border-bottom-left-radius: 5px;
+        border-top-width: 2px;
+        border-right-width: 2px;
+        border-bottom-width: 2px;
+        border-left-width: 2px;
+        border-top-style: none;
+        border-right-style: none;
+        border-bottom-style: none;
+        border-left-style: none;
+        border-top-color: #767676;
+        border-right-color: #767676;
+        border-bottom-color: #767676;
+        border-left-color: #767676;
+        border-image-source: initial;
+        border-image-slice: initial;
+        border-image-width: initial;
+        border-image-outset: initial;
+        border-image-repeat: initial;
+        padding-top: 1px;
+        padding-right: 16px;
+        padding-bottom: 1px;
+        padding-left: 16px;
+        font-size: 16px;
+        }
+
+        .arch_contact_us_duplicate .textinput {
+        width: 100%;
+        height: 125px;
+        margin-top: 0px;
+        margin-right: 0px;
+        margin-bottom: 27px;
+        margin-left: 0px;
+        border-top-width: 1px;
+        border-right-width: 1px;
+        border-bottom-width: 1px;
+        border-left-width: 1px;
+        border-top-style: none;
+        border-right-style: none;
+        border-bottom-style: none;
+        border-left-style: none;
+        border-top-color: #767676;
+        border-right-color: #767676;
+        border-bottom-color: #767676;
+        border-left-color: #767676;
+        border-image-source: initial;
+        border-image-slice: initial;
+        border-image-width: initial;
+        border-image-outset: initial;
+        border-image-repeat: initial;
+        font-size: 16px;
+        padding-top: 16px;
+        padding-right: 16px;
+        padding-bottom: 16px;
+        padding-left: 16px;
+        }
+
+        .arch_contact_us_duplicate .button {
+        height: 50px;
+        min-width: 240px;
+        font-size: 16px;
+        color: white;
+        border-top-width: 2px;
+        border-right-width: 2px;
+        border-bottom-width: 2px;
+        border-left-width: 2px;
+        border-top-style: none;
+        border-right-style: none;
+        border-bottom-style: none;
+        border-left-style: none;
+        border-top-color: #767676;
+        border-right-color: #767676;
+        border-bottom-color: #767676;
+        border-left-color: #767676;
+        border-image-source: initial;
+        border-image-slice: initial;
+        border-image-width: initial;
+        border-image-outset: initial;
+        border-image-repeat: initial;
+        margin-top: 0px;
+        margin-right: 0px;
+        margin-bottom: 60px;
+        margin-left: 0px;
+        background-color: #03DF04;
+        }
+
+        .arch_contact_us_duplicate .form-wrapper {
+        border-bottom-width: 4px;
+        border-bottom-style: solid;
+        border-bottom-color: #f3f2cf;
+        margin-top: 0px;
+        margin-right: 0px;
+        margin-bottom: 60px;
+        margin-left: 0px;
+        }
+
+        .arch_contact_us_duplicate .social-media-icon-container {
+        display: flex;
+        max-width: 400px;
+        justify-content: space-evenly;
+        }
+
+        .arch_contact_us_duplicate .input-element {
+        background-color: #adf7ad;
+        }
+
+        .arch_contact_us_duplicate .icon-block.social-icon {
+        fill: ;
+        }
+
+        @media (max-width: 1024px) {
+        .arch_contact_us_duplicate .text-blk.section-head {
+            font-size: 55px;
+            line-height: 60px;
+        }
+
+        .arch_contact_us_duplicate .responsive-container-block.container {
+            justify-content: space-between;
+        }
+        }
+
+        @media (max-width: 768px) {
+        .arch_contact_us_duplicate .responsive-cell-block.wk-tab-12.wk-mobile-12.wk-ipadp-6.wk-desk-5 {
+            margin-top: 0px;
+            margin-right: 0px;
+            margin-bottom: 80px;
+            margin-left: 0px;
+        }
+
+        .arch_contact_us_duplicate .responsive-cell-block.wk-mobile-12.wk-ipadp-6.wk-desk-5.wk-tab-10 {
+            margin-top: 0px;
+            margin-right: 0px;
+            margin-bottom: 80px;
+            margin-left: 0px;
+            text-align: center;
+        }
+
+        .arch_contact_us_duplicate .input {
+            height: 45px;
+        }
+
+        .arch_contact_us_duplicate .responsive-container-block.container {
+            justify-content: space-evenly;
+            margin-top: 80px;
+            margin-right: 0px;
+            margin-bottom: 80px;
+            margin-left: 0px;
+        }
+
+        .arch_contact_us_duplicate .responsive-cell-block.wk-mobile-12.wk-desk-5.wk-tab-10.wk-ipadp-5 {
+            margin-top: 0px;
+            margin-right: 0px;
+            margin-bottom: 80px;
+            margin-left: 0px;
+        }
+        }
+
+        @media (max-width: 500px) {
+        .arch_contact_us_duplicate .text-blk.section-head {
+            font-size: 40px;
+            line-height: 45px;
+        }
+
+        .arch_contact_us_duplicate .responsive-container-block.big-container {
+            padding-top: 0px;
+            padding-right: 20px;
+            padding-bottom: 0px;
+            padding-left: 20px;
+        }
+
+        .arch_contact_us_duplicate .input {
+            height: 40px;
+        }
+
+        .arch_contact_us_duplicate .responsive-cell-block.wk-tab-12.wk-mobile-12.wk-ipadp-6.wk-desk-5 {
+            margin-top: 0px;
+            margin-right: 0px;
+            margin-bottom: 80px;
+            margin-left: 0px;
+        }
+
+        .arch_contact_us_duplicate .responsive-cell-block.wk-mobile-12.wk-ipadp-6.wk-desk-5.wk-tab-10 {
+            text-align: left;
+            padding-top: 0px;
+            padding-right: 20px;
+            padding-bottom: 0px;
+            padding-left: 20px;
+        }
+
+        .arch_contact_us_duplicate .responsive-container-block.big-container {
+            padding-top: 0px;
+            padding-right: 30px;
+            padding-bottom: 0px;
+            padding-left: 30px;
+        }
+
+        .arch_contact_us_duplicate .responsive-cell-block.wk-mobile-12.wk-desk-5.wk-tab-10.wk-ipadp-5 {
+            padding-top: 0px;
+            padding-right: 20px;
+            padding-bottom: 0px;
+            padding-left: 20px;
+        }
+
+        .arch_contact_us_duplicate .button {
+            width: 100%;
+        }
+
+        .arch_contact_us_duplicate .button {
+            margin-top: 0px;
+            margin-right: 0px;
+            margin-bottom: 40px;
+            margin-left: 0px;
+            height: 50px;
+        }
+
+        .arch_contact_us_duplicate .form-wrapper {
+            margin-top: 0px;
+            margin-right: 0px;
+            margin-bottom: 40px;
+            margin-left: 0px;
+        }
+
+        .arch_contact_us_duplicate .icon-block {
+            width: 25px;
+            height: 25px;
+        }
+        }
+           @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700;800&amp;display=swap');
+
+            *,
+            *:before,
+            *:after {
+            -moz-box-sizing: border-box;
+            -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+            }
+
+            body {
+            margin: 0;
+            }
+
+            .wk-desk-1 {
+            width: 8.333333%;
+            }
+
+            .wk-desk-2 {
+            width: 16.666667%;
+            }
+
+            .wk-desk-3 {
+            width: 25%;
+            }
+
+            .wk-desk-4 {
+            width: 33.333333%;
+            }
+
+            .wk-desk-5 {
+            width: 41.666667%;
+            }
+
+            .wk-desk-6 {
+            width: 50%;
+            }
+
+            .wk-desk-7 {
+            width: 58.333333%;
+            }
+
+            .wk-desk-8 {
+            width: 66.666667%;
+            }
+
+            .wk-desk-9 {
+            width: 75%;
+            }
+
+            .wk-desk-10 {
+            width: 83.333333%;
+            }
+
+            .wk-desk-11 {
+            width: 91.666667%;
+            }
+
+            .wk-desk-12 {
+            width: 100%;
+            }
+
+            @media (max-width: 1024px) {
+            .wk-ipadp-1 {
+                width: 8.333333%;
+            }
+
+            .wk-ipadp-2 {
+                width: 16.666667%;
+            }
+
+            .wk-ipadp-3 {
+                width: 25%;
+            }
+
+            .wk-ipadp-4 {
+                width: 33.333333%;
+            }
+
+            .wk-ipadp-5 {
+                width: 41.666667%;
+            }
+
+            .wk-ipadp-6 {
+                width: 50%;
+            }
+
+            .wk-ipadp-7 {
+                width: 58.333333%;
+            }
+
+            .wk-ipadp-8 {
+                width: 66.666667%;
+            }
+
+            .wk-ipadp-9 {
+                width: 75%;
+            }
+
+            .wk-ipadp-10 {
+                width: 83.333333%;
+            }
+
+            .wk-ipadp-11 {
+                width: 91.666667%;
+            }
+
+            .wk-ipadp-12 {
+                width: 100%;
+            }
+            }
+
+            @media (max-width: 768px) {
+            .wk-tab-1 {
+                width: 8.333333%;
+            }
+
+            .wk-tab-2 {
+                width: 16.666667%;
+            }
+
+            .wk-tab-3 {
+                width: 25%;
+            }
+
+            .wk-tab-4 {
+                width: 33.333333%;
+            }
+
+            .wk-tab-5 {
+                width: 41.666667%;
+            }
+
+            .wk-tab-6 {
+                width: 50%;
+            }
+
+            .wk-tab-7 {
+                width: 58.333333%;
+            }
+
+            .wk-tab-8 {
+                width: 66.666667%;
+            }
+
+            .wk-tab-9 {
+                width: 75%;
+            }
+
+            .wk-tab-10 {
+                width: 83.333333%;
+            }
+
+            .wk-tab-11 {
+                width: 91.666667%;
+            }
+
+            .wk-tab-12 {
+                width: 100%;
+            }
+            }
+
+            @media (max-width: 500px) {
+            .wk-mobile-1 {
+                width: 8.333333%;
+            }
+
+            .wk-mobile-2 {
+                width: 16.666667%;
+            }
+
+            .wk-mobile-3 {
+                width: 25%;
+            }
+
+            .wk-mobile-4 {
+                width: 33.333333%;
+            }
+
+            .wk-mobile-5 {
+                width: 41.666667%;
+            }
+
+            .wk-mobile-6 {
+                width: 50%;
+            }
+
+            .wk-mobile-7 {
+                width: 58.333333%;
+            }
+
+            .wk-mobile-8 {
+                width: 66.666667%;
+            }
+
+            .wk-mobile-9 {
+                width: 75%;
+            }
+
+            .wk-mobile-10 {
+                width: 83.333333%;
+            }
+
+            .wk-mobile-11 {
+                width: 91.666667%;
+            }
+
+            .wk-mobile-12 {
+                width: 100%;
+            }
+            } 
+    </style>
+</head>
+<body>
+        <div class="arch_contact_us_duplicate">
+        <div class="responsive-container-block big-container">
+            <div class="responsive-container-block container">
+            <div class="responsive-cell-block wk-mobile-12 wk-desk-5 wk-tab-10 wk-ipadp-5" id="ih6s">
+                <p class="text-blk section-head">
+                Get in touch dolor sit
+                </p>
+                <p class="text-blk section-subhead">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                </p>
+            </div>
+            <div class="responsive-cell-block wk-ipadp-6 wk-mobile-12 wk-desk-5 wk-tab-9" id="i6df">
+                <div class="form-wrapper">
+                <input class="input input-element" name="Name" placeholder="Name">
+                <input class="input input-element" name="Contact Number" placeholder="Contact Number">
+                <input class="input input-element" name="Email" placeholder="Email">
+                <textarea class="textinput input-element" placeholder="Message"></textarea>
+                <button class="button">
+                    Send
+                </button>
+                </div>
+                <div class="social-media-icon-container">
+                <div class="icon-block social-icon">
+                    <svg height="37.7" id="ihxs6" viewBox="0 0 37.701 37.7" width="37.701" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M32.178,0H5.523A5.529,5.529,0,0,0,0,5.522V32.178A5.529,5.529,0,0,0,5.523,37.7H16.641V24.372H12.223V17.746h4.418V13.254a6.634,6.634,0,0,1,6.627-6.627h6.7v6.627h-6.7v4.492h6.7l-1.1,6.627h-5.6V37.7h8.91A5.529,5.529,0,0,0,37.7,32.178V5.522A5.529,5.529,0,0,0,32.178,0Zm0,0">
+                    </path>
+                    </svg>
+                </div>
+                <div class="icon-block social-icon">
+                    <svg height="37.7" id="inclm" viewBox="0 0 37.701 37.7" width="37.701" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M37.587,11.078A13.839,13.839,0,0,0,36.711,6.5a9.238,9.238,0,0,0-2.174-3.339A9.241,9.241,0,0,0,31.2.989,13.837,13.837,0,0,0,26.622.114C24.612.022,23.969,0,18.85,0s-5.762.022-7.772.113A13.841,13.841,0,0,0,6.5.99,9.239,9.239,0,0,0,3.164,3.164,9.24,9.24,0,0,0,.989,6.5a13.838,13.838,0,0,0-.875,4.576C.022,13.088,0,13.73,0,18.85s.022,5.762.114,7.772A13.835,13.835,0,0,0,.99,31.2a9.237,9.237,0,0,0,2.174,3.339A9.236,9.236,0,0,0,6.5,36.71a13.832,13.832,0,0,0,4.576.876c2.011.092,2.653.113,7.772.113s5.762-.022,7.772-.113A13.834,13.834,0,0,0,31.2,36.71,9.637,9.637,0,0,0,36.711,31.2a13.834,13.834,0,0,0,.876-4.576c.091-2.011.113-2.653.113-7.772s-.022-5.761-.113-7.772ZM34.194,26.467a10.429,10.429,0,0,1-.649,3.5,6.242,6.242,0,0,1-3.578,3.578,10.43,10.43,0,0,1-3.5.649c-1.987.091-2.584.11-7.617.11s-5.63-.019-7.617-.11a10.433,10.433,0,0,1-3.5-.649,5.842,5.842,0,0,1-2.167-1.41,5.839,5.839,0,0,1-1.41-2.167,10.429,10.429,0,0,1-.649-3.5c-.091-1.988-.11-2.584-.11-7.617s.019-5.629.11-7.617a10.437,10.437,0,0,1,.649-3.5,5.844,5.844,0,0,1,1.41-2.168,5.836,5.836,0,0,1,2.168-1.41,10.428,10.428,0,0,1,3.5-.649c1.988-.091,2.584-.11,7.617-.11h0c5.033,0,5.63.019,7.617.11a10.431,10.431,0,0,1,3.5.649,5.845,5.845,0,0,1,2.167,1.41,5.836,5.836,0,0,1,1.41,2.168,10.42,10.42,0,0,1,.649,3.5c.091,1.988.11,2.584.11,7.617S34.285,24.479,34.194,26.467Zm0,0" transform="translate(0 0)">
+                    </path>
+                    <path d="M134.219,124.539a9.68,9.68,0,1,0,9.68,9.68A9.68,9.68,0,0,0,134.219,124.539Zm0,15.963a6.283,6.283,0,1,1,6.284-6.284A6.283,6.283,0,0,1,134.219,140.5Zm0,0" transform="translate(-115.369 -115.369)">
+                    </path>
+                    <path d="M366.454,90.888a2.262,2.262,0,1,1-2.262-2.262A2.262,2.262,0,0,1,366.454,90.888Zm0,0" transform="translate(-335.279 -82.1)">
+                    </path>
+                    </svg>
+                </div>
+                <div class="icon-block social-icon">
+                    <svg height="33.932" id="ijipc" viewBox="0 0 41.754 33.932" width="41.754" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M66.857,38.045a17.092,17.092,0,0,1-4.92,1.347A8.589,8.589,0,0,0,65.7,34.654a17.149,17.149,0,0,1-5.44,2.078,8.573,8.573,0,0,0-14.6,7.814A24.317,24.317,0,0,1,28.009,35.6,8.575,8.575,0,0,0,30.66,47.032a8.506,8.506,0,0,1-3.88-1.073c0,.036,0,.073,0,.109a8.571,8.571,0,0,0,6.872,8.4,8.6,8.6,0,0,1-3.868.148,8.575,8.575,0,0,0,8,5.949A17.293,17.293,0,0,1,25.1,64.111a24.231,24.231,0,0,0,13.13,3.849c15.756,0,24.373-13.053,24.373-24.373,0-.371-.008-.741-.025-1.108a17.371,17.371,0,0,0,4.275-4.434Z" transform="translate(-25.103 -34.028)">
+                    </path>
+                    </svg>
+                </div>
+                <div class="icon-block social-icon">
+                    <svg height="37.649" id="ij4qf" viewBox="0 0 39.398 37.649" width="39.398" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M66.466,47.05V61.618H58.02V48.026c0-3.413-1.219-5.743-4.278-5.743a4.62,4.62,0,0,0-4.332,3.088,5.781,5.781,0,0,0-.28,2.058V61.617H40.684s.113-23.02,0-25.4h8.447v3.6c-.017.028-.041.056-.056.083h.056v-.083a8.386,8.386,0,0,1,7.612-4.2c5.557,0,9.723,3.631,9.723,11.432ZM31.848,23.969c-2.889,0-4.78,1.9-4.78,4.388a4.374,4.374,0,0,0,4.669,4.39h.055c2.946,0,4.778-1.951,4.778-4.39a4.388,4.388,0,0,0-4.722-4.388ZM27.57,61.618h8.444v-25.4H27.57Z" transform="translate(-27.068 -23.969)">
+                    </path>
+                    </svg>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div> 
+</body>
+</html>
