0179d167853cec746b4097da932876852e20003d
Palak-16 <79013214+Palak-16@users.noreply.github.com>
2023-08-01 12:13:33 +0530

    state

--- apps/pages/signup.php
+++ apps/pages/signup.php
@@ -223,6 +223,39 @@ function old_checked($key)
       </select>
       <label for="floatingInput">gender</label>  
     </div>
+    <div class="form-floating">
+      <select value="<?=old_value('state')?>" name="state" type="text" class="form-control mb-2" id="floatingInput" placeholder="state">
+        <option value="Andhra Pradesh">Andhra Pradesh</option>
+	      <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+	      <option value="Assam">Assam</option>
+	      <option value="Bihar">Bihar</option>
+	      <option value="Chhattisgarh">Chhattisgarh</option>
+	      <option value="Goa">Goa</option>
+	      <option value="Gujarat">Gujarat</option>
+	      <option value="Haryana">Haryana</option>
+	      <option value="Himachal Pradesh">Himachal Pradesh</option>
+	      <option value="Jharkhand">Jharkhand</option>
+	      <option value="Karnataka">Karnataka</option>
+	      <option value="Kerala">Kerala</option>
+	      <option value="Madhya Pradesh">Madhya Pradesh</option>
+	      <option value="Maharashtra">Maharashtra</option>
+	      <option value="Manipur">Manipur</option>
+	      <option value="Meghalaya">Meghalaya</option>
+	      <option value="Mizoram">Mizoram</option>
+	      <option value="Nagaland">Nagaland</option>
+	      <option value="Odisha">Odisha</option>
+	      <option value="Punjab">Punjab</option>
+	      <option value="Rajasthan">Rajasthan</option>
+	      <option value="Sikkim">Sikkim</option>
+	      <option value="Tamil Nadu">Tamil Nadu</option>
+	      <option value="Telangana">Telangana</option>
+	      <option value="Tripura">Tripura</option>
+	      <option value="Uttar Pradesh">Uttar Pradesh</option>
+	      <option value="Uttarakhand">Uttarakhand</option>
+	      <option value="West Bengal">West Bengal</option>
+      </select>  
+      <label for="floatingInput">state</label> 
+    </div>
 
     <div class="form-floating">
       <input value="<?=old_value('password')?>" name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
